a3bef947dbbb2dfe0a0c79673407b185f67f264d
shonbase12 <shonbase12@gmail.com>
2026-04-30 14:03:36 +0300

    Enhance PaymentException with cause chaining constructors and override toString() for better logging

--- src/model/PaymentException.java
+++ src/model/PaymentException.java
@@ -8,5 +8,25 @@ public class PaymentException extends RuntimeException {
         this.errorCode = errorCode;
     }
 
-    public String getErrorCode() { return errorCode; }
+    public PaymentException(String message, String errorCode, Throwable cause) {
+        super(message, cause);
+        this.errorCode = errorCode;
+    }
+
+    public PaymentException(String errorCode, Throwable cause) {
+        super(cause);
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorCode() { 
+        return errorCode; 
+    }
+
+    @Override
+    public String toString() {
+        return "PaymentException{" +
+                "errorCode='" + errorCode + '\'' +
+                ", message='" + getMessage() + '\'' +
+                '}';
+    }
 }
