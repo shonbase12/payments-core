f25f9c290630b9b744d46fe69a04a2efd9824aa2
shonbase12 <shonbase12@gmail.com>
2026-04-30 23:20:27 +0300

    Add structured logging support in PaymentException

--- src/model/PaymentException.java
+++ src/model/PaymentException.java
@@ -1,11 +1,16 @@
 package com.novapay.payments.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class PaymentException extends RuntimeException {
+    private static final Logger logger = LoggerFactory.getLogger(PaymentException.class);
     private final String errorCode;
 
     public PaymentException(String message, String errorCode) {
         super(message);
         this.errorCode = errorCode;
+        logger.error("PaymentException created with errorCode: {} and message: {}", errorCode, message);
     }
 
     public String getErrorCode() { return errorCode; }
