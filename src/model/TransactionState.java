8cfe8475a4725424d0b423a6ac35cb17b8261c9f
shonbase12 <shonbase12@gmail.com>
2026-04-30 23:20:35 +0300

    Add utility method for logging state transitions in TransactionState enum

--- src/model/TransactionState.java
+++ src/model/TransactionState.java
@@ -1,9 +1,18 @@
 package com.novapay.payments.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public enum TransactionState {
     PENDING,
     PROCESSING,
     COMPLETED,
     FAILED;
     // Add additional states as necessary
+
+    private static final Logger logger = LoggerFactory.getLogger(TransactionState.class);
+
+    public void logTransition(TransactionState fromState, String transactionId) {
+        logger.info("Transaction {} state changed from {} to {}", transactionId, fromState, this);
+    }
 }
